39383f6a16b57d1a021a64c62aea7755182e6f04
Bennyboy46 <Darshanbenedict46@gmail.com>
2025-01-04 20:41:45 +0530

    added files

---
+++ main.py
@@ -0,0 +1,73 @@
+import cv2
+import time
+import numpy as np
+import HandTrackingModule as htm
+import math
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+
+wCam, hCam = 640, 480
+
+cap = cv2.VideoCapture(0)  # Use 0 for the default webcam
+cap.set(3, wCam)
+cap.set(4, hCam)
+pTime = 0
+
+detector = htm.handDetector(detectionCon=0.7)
+
+# Audio control setup
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(
+    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+volRange = volume.GetVolumeRange()
+minVol = volRange[0]
+maxVol = volRange[1]
+vol = 0
+volBar = 400
+volPer = 0
+
+while True:
+    success, img = cap.read()
+    img = detector.findHands(img)
+    lmList, _ = detector.findPosition(img)
+
+    if len(lmList) != 0:
+        # Get thumb and index positions
+        x1, y1 = lmList[4][1], lmList[4][2]  # Thumb tip
+        x2, y2 = lmList[8][1], lmList[8][2]  # Index tip
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2  # Center of the line
+
+        # Calculate the distance between thumb and index finger
+        length = math.hypot(x2 - x1, y2 - y1)
+
+        # Map the length to volume
+        vol = np.interp(length, [50, 300], [minVol, maxVol])
+        volBar = np.interp(length, [50, 300], [400, 150])
+        volPer = np.interp(length, [50, 300], [0, 100])
+        volume.SetMasterVolumeLevel(vol, None)
+
+        # Visual feedback
+        if length < 50:
+            cv2.circle(img, (cx, cy), 15, (0, 255, 0), cv2.FILLED)
+
+    # Display volume bar and percentage
+    cv2.rectangle(img, (50, 150), (85, 400), (255, 0, 0), 3)
+    cv2.rectangle(img, (50, int(volBar)), (85, 400), (255, 0, 0), cv2.FILLED)
+    cv2.putText(img, f'{int(volPer)} %', (40, 450), cv2.FONT_HERSHEY_COMPLEX,
+                1, (255, 0, 0), 3)
+
+    # Show FPS
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+    cv2.putText(img, f'FPS: {int(fps)}', (40, 50), cv2.FONT_HERSHEY_COMPLEX,
+                1, (255, 0, 0), 3)
+
+    cv2.imshow("Image", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):  # Press 'q' to exit
+        break
+
+cap.release()
+cv2.destroyAllWindows()
